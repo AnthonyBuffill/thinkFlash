e9fa79c78d2aac053826637417ba3c236d9800f8
Karen Douglas <kdindependence@gmail.com>
2024-03-07 13:09:24 -0600

    set up the routes with react router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.jsx'
+//pages
+import AboutPage from './pages/AboutPage.jsx'
+import DashboardPage from './pages/DashboardPage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import SignupPage from './pages/SignupPage.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const router = (
+  <Routes>
+    <Route path="/" element={<App />}>
+      <Route index element={<AboutPage />} />
+      <Route path="dashboard" element={<DashboardPage/>} />
+      <Route path="dashboard/:deckId" element={<DashboardPage/>} />
+      <Route path="login" element={<LoginPage />} />
+      <Route path="signup" element={<SignupPage/>} />
+    </Route>
+  </Routes>
+);
+
+
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Router>{router}</Router>
+  </React.StrictMode>
+);
+
