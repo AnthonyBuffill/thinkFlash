eb89f0c20dc89bb4e8dfe09d2c53e2e5d5419c69
Karen Douglas <kdindependence@gmail.com>
2024-03-07 14:19:56 -0600

    styled navbar for small screens

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
+import { Link } from "react-router-dom"
+import '../assets/css/navbar.css'
+
 
 export default function Navbar() {
+    const menuOpen = '../assets/tfmenu.png'
+    const menuClose = '../assets/tfcloseMenu.png'
+    const [menuBtn, setMenuButton]  = useState(menuOpen)
+    const [showMenu, setShowMenu]  = useState(false)
+
+    const handleMenuToggle = () => {
+       
+        if(menuBtn === menuOpen){
+            setMenuButton(menuClose)
+            setShowMenu(true)
+        }else{
+            setMenuButton(menuOpen)
+            setShowMenu(false)
+        }
+
+
+    }
     return (
         <>
-        <nav>
-            
+        <nav className="nav-container">
+            <section className="logo-container">
+                <button className="menu-btn" onClick={handleMenuToggle}>
+                    <img src={menuBtn} alt="" />
+                </button>
+                <div>Logo</div>
+            </section>
+            {showMenu &&
+            <section className="nav-links-container">
+                <button>
+                    <Link to={'/'}>ABOUT</Link>
+                </button>
+                <button>
+                    <Link to={'/dashboard'}>DASHBOARD</Link>
+                </button>
+                <button>
+                    <Link to={'/login'}>LOG IN</Link>
+                </button>
+                <button>
+                    <Link to={'/signup'}>SIGN UP</Link>
+                </button>
+            </section>
+        }
         </nav>
         </>
     )
