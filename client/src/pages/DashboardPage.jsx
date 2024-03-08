39858e1c45836834dbefea406574616a6e197e2e
Karen Douglas <kdindependence@gmail.com>
2024-03-08 10:59:32 -0600

    styled the dashboard nav middle bar for small screens

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-
+import '../assets/css/dashboard.css'
 export default function DashboardPage() {
     return (
         <>
-        <div>DASHBOARD</div>
+        <main>
+            <section className="dash-nav-container">
+                <button>
+                    <a href="/addCard">+ add card</a>
+                </button>
+            </section>
+            ------------------------
+            <section>Decks</section>
+            
+        </main>
         </>
     )
 }
