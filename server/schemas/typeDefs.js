f92f7e276da59d7d1ea420faa64ea50036918d6c
Ant Buffill <abuffill@outlook.com>
2024-03-07 22:56:29 -0600

    checking

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,26 @@
 const typeDefs = `
-  type Query {
-    number: Int
-  }
+type User {
+  _id: ID
+  username: String
+  email: String
+  password: String
+  decks: [Deck]!
+}
+type Deck {
+  _id: ID
+  title: String
+  description: String
+  cards: [Card]!
+  createdAt: String
+}
+Card {
+_Id : Int
+fronttext : String
+backtext: String
+CreatedAt: String
+}
 `;
+
+module.exports = typeDefs;
 
 module.exports = typeDefs;
