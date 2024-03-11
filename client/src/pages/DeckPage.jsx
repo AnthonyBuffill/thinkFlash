9b023293ab8067a79790f9b91f3a0cbaed6fcdab
Karen Douglas <kdindependence@gmail.com>
2024-03-11 13:15:28 -0500

    styled card components with flip card animations

--- client/src/pages/DeckPage.jsx
+++ client/src/pages/DeckPage.jsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
+import '../assets/css/deck.css'
+
+
+function Card() {
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const handleFlipCard = () => {
+        setIsFlipped(!isFlipped);
+    };
+
+    return (
+        <section className="deck-flip-card-container" onClick={handleFlipCard}>
+            <div className={`flip-card-inner ${isFlipped ? "flipped" : ""}`} >
+                <figure className="flip-card-front">
+                    <h3>Front Card Question</h3>
+                </figure>
+                <figure className="flip-card-back">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet accusantium cupiditate voluptatibus laborum unde dolorem, alias, fugiat sint minima praesentium odit delectus doloremque earum, recusandae commodi excepturi? Voluptatem, commodi obcaecati!</p>
+                </figure>
+            </div>
+        </section>
+    );
+}
 
 export default function DeckPage() {
+
     return (
         <>
         <main>
@@ -13,7 +37,17 @@ export default function DeckPage() {
                 </button>
             </section>
 
-            <section>CARDS</section>
+            <section  className="cards-container">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+            </section>
             
         </main>
         </>
